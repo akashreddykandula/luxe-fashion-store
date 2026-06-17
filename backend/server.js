8645225e8de0881e1fc97ae563c8f8819176db16
akashreddykandula <akashreddykandula@gmail.com>
2026-06-17 16:50:24 +0530

    Fix production CORS

--- backend/server.js
+++ backend/server.js
@@ -72,9 +72,21 @@ app.use (express.urlencoded ({extended: true, limit: '10mb'}));
 //   allowedHeaders: ['Content-Type', 'Authorization']
 // }));
 
+// app.use (
+//   cors ({
+//     origin: process.env.CLIENT_URL || 'http://localhost:5173',
+//     credentials: true,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+//   })
+// );
 app.use (
   cors ({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'https://luxe-fashion-store-six.vercel.app',
+      'https://luxe-fashion-store-hp0yxlzkt-akash-kandulas-projects.vercel.app',
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
